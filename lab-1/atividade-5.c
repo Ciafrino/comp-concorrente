9bf3e4e47c8c1d3e2bffb9423a333146497663bf
ciafrino <chrisciafrino@ufrj.br>
2021-07-17 17:39:44 -0300

    Atualizando lab-1

--- lab-1/atividade-5.c
+++ lab-1/atividade-5.c
@@ -1,8 +1,15 @@
+// Me baseei no código construido com a turma na aula assíncrona 3
+// para implementar a atividade 5 do lab-1
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-#include <assert.h>
+#include<assert.h>
 
+// Vamos construir duas threads, uma par e outra ímpar
+// A par vai iterar pelos elementos de índice par, da forma
+// [2, 4, ..., N], já na thread ímpar vamos fazer [1, 3, ..., N-1]
+// Para fazer isso podemos iterar com 2 * id + paridade, pra todo id
+// em [0, N/2) com paridade sendo 1 se a thread for ímpar e 0 se for par
 const short NTHREADS = 2;
 
 typedef struct {
@@ -11,25 +18,29 @@ typedef struct {
    int parity; // 1 se for ímpar. 0 se for par
 } data_t;
 
-const int N = 10000; 
+const int N = 10000; // quantidade de elementos no array
 
 int values[100010];
 
-// retorna o indice correspondente do vetor dependendo da paridade
-// Se for a thread par, quero calcular para os indices pares
-// E análogo caso a thread seja ímpar
+// retorna o indice correspondente do array dependendo da paridade
+// Se for a thread par, quero calcular para os indices pares (2 * indice)
+// E análogo caso a thread seja ímpar (2 * indice + 1)
 int get_index(int i, int parity) {
-    return i << 1 | (parity & 1);
+    return 2 * i + parity;
 }
 
-// eleva ao quadrado os elementos do array values de índices de certa paridade
+// eleva ao quadrado os elementos de índices pares ou ímpares do array values 
 void *square(void *arg) {
     data_t* cur_thread = (data_t *)arg;
     
-    for (int i = 0; i < (N >> 1); ++i) {
+    printf("\nEstamos na thread %d!\n", cur_thread->parity);
+
+    for (int i = 0; i < (N / 2); ++i) {
         int p = get_index(i, cur_thread->parity);
         values[p] *= values[p];
     }
+   
+    printf("\nSaimos da thread %d!\n", cur_thread->parity);
 
     free(arg);
     pthread_exit(NULL);
@@ -48,33 +59,34 @@ int main() {
     for (int id = 0; id < NTHREADS; ++id) {
         arg = malloc(sizeof(data_t));
         if (arg == NULL) {
-            printf("--ERRO: malloc\n");
+            printf("--ERRO: malloc()\n");
             exit(-1);
         }
 
         arg->id_thread = id;
         arg->cnt_thread = NTHREADS; 
-        arg->parity = id; // paridade da thread
+        arg->parity = (id & 1); // paridade da thread
 
+        // cria a thread id
         if (pthread_create(&tid[id], NULL, square, (void*)arg)) {
-            printf("--ERRO: pthread_create()\n");
+            printf("--ERRO: pthread_create() %d\n", id);
             exit(-1);
         }
     }
    
-    for (int i = 0; i < NTHREADS; ++i) {
-        if (pthread_join(tid[i], NULL)) {
-            printf("--ERRO: pthrad_join()\n");
+    for (int id = 0; id < NTHREADS; ++id) {
+        // espera a thread i acabar
+        if (pthread_join(tid[id], NULL)) {
+            printf("--ERRO: pthread_join() %d\n", id);
             exit(-1);
         }
     }
     
+    // Erro de execução se o resultado não bater com o esperado
     for (int i = 0; i < N; ++i) {
         assert(values[i] == (i + 1) * (i + 1));
-        // printf("%d and %d\n", values[i], (i + 1) * (i + 1));
-        // fflush(stdin);
     }
 
-    printf("Passou no teste!\n");
+    printf("\n\nPassou no teste!\n");
     pthread_exit(NULL);
 }
